81116314dfc39fff0c44e1e8d99c63a064ce958f
Brett Sira <brett@Bretts-MacBook-Pro.local>
2022-10-16 14:39:18 +1100

    clearn up functions

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
 import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
-import "./App.css";
+import "./App.css"
 
 const App = () => {
-  // use state variable monsters
+  // use state monsters variable
   const [monsters, setMonsters] = useState([]);
+  // use state search field variable
+  const [searchField, setSearchField] = useState("");
+  // use filtered monster variable
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  // use state variable search field
-  const [searchField, setSearchField] = useState(""); // [vlue, setVaule]
-
-  // use state variable filtered monsters
-  const [filteredMonsters, setfilteredMonsters] = useState(monsters);
-
-  // use effect set initial variable of monsters
+  // use effect initialize monster data
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((res) => {
-        setMonsters(res);
+      .then((monsters) => {
+        setMonsters(monsters);
       });
-  }, []); // [Func; (it will run if dependency arr had change), arrOfDependency]
+  }, []);
 
-  // use effect filter monsters
+  // func search monster cards; set state of search field 
+  const searchMonsters = (event) => {
+    setSearchField(event.target.value) 
+  }
+
+  // use effect tricker execute filter if searchField value has change
   useEffect(() => {
-    const newFilteredMonsters = monsters.filter((monster) =>
-      monster.name.toLocaleLowerCase().includes(searchField)
-    );
-    setfilteredMonsters(newFilteredMonsters)
-  }, [monsters, searchField])
+    const filteredNameOfMonsters = monsters.filter(monster => (
+      monster.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase())
+    ))
+    setFilteredMonsters(filteredNameOfMonsters)
+  }, [searchField, monsters])
 
-  const searchMonstersOnChange = (e) => {
-    const searchFieldString = e.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
-  };
-
-
-  return (
+  console.log(monsters)
+  return(
     <div className="App">
-      <h1 className="app-title">APP rendered</h1>
-      <SearchBox
-        className="monsters-search-box"
-        onChangeHandler={searchMonstersOnChange}
-        placeholder="Seatch monster"
-      />
-      <CardList monsters={filteredMonsters} />
-    </div>
-  );
+      <h1 className="app-title">Monster Dex</h1>
+      <SearchBox className="monsters-search-box"
+      onChangeHandler={searchMonsters}
+      placeholder="Monster Loop Up"/>
+      <CardList monsters={filteredMonsters}/>
+    </div>);
 };
 
 export default App;
