d079f36d34eba4d8aa87f2daf74f64fbf5d9364d
Brett Sira <brett@Bretts-MacBook-Pro.local>
2022-10-16 22:52:11 +1100

    cardlist to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,26 +1,21 @@
-import { Component } from "react";
-import Card from '../card/card.component'
-import './card-list.style.css'
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    console.log(this.props.monsters);
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          let { name, id, email } = monster;
-          return (
-            <Card 
+import Card from "../card/card.component";
+import "./card-list.style.css";
+
+const CardList = ({monsters}) => {
+  return (
+    <div className="card-list">
+      {monsters.map(({ name, id, email }) => (
+          <Card
+            key={id}
             id={id}
             name={name}
             email={email}
             src={`https://robohash.org/${id}?set=set2`}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+          />
+        )
+      )}
+    </div>
+  );
+};
 
 export default CardList;
